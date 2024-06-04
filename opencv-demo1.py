5280f0883c7a145eab86e8d23a2c0f4c12620eb6
itmanharry <jack_chengqian@163.com>
2024-06-04 16:06:13 +0800

    img properties and functions

--- opencv-demo1.py
+++ opencv-demo1.py
@@ -4,6 +4,7 @@ img = cv.imread(r'E:\Images\CGQ.jpg')
 if img is None:
     sys.exit('Could not read the image!')
 print('-' * 80)
+print(img)
 # shape property
 dimensions = img.shape
 print(dimensions)
@@ -23,6 +24,17 @@ print('-' * 80)
 dtype = img.dtype
 print('Data type is : ', dtype)
 print('-' * 80)
+# some pixel channel values
+b, g, r = img[6, 40]
+print(f'Channel values are , Blue {b}, Green {g}, Red {r}.')
+b, g, r = img[480, 40]
+print(f'Channel values are , Blue {b}, Green {g}, Red {r}.')
+print(f'Channel values are , Blue {img[480, 40, 0]}, Green {img[480, 40, 1]}, Red {img[480, 40, 2]}.')
+print('-' * 80)
+# set channel values
+img[10, 30] = (0, 0, 255)
+top_corner = img[0:50, 0:50]
+print('Top corner type is : ', type(top_corner))
 # show image
 cv.imshow('My Photo', img)
 k = cv.waitKey(0)
